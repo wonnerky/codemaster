fb6ec6ba5cbdd5f3c93d80bd7bc8892e3443d079
dc5000pcx <dc5000pcx@hmail.com>
2020-04-21 14:50:37 +0900

    200421_04_ps

--- ps/15 backtracking/1(15649).py
+++ ps/15 backtracking/1(15649).py
@@ -2,17 +2,4 @@ import sys
 
 a, b = map(int, sys.stdin.readline().split())
 
-<<<<<<< HEAD
-for i in range(a):
-    for j in range(b):
-        if i !=
-=======
-x = 0
 
-for i in range(a):
-    x += 1
-    y = 0
-    for j in range(b):
-        if x != j+1:
-            print(j+1, end=" ")
->>>>>>> 8893303b5576737fdd24129b31c136a7fd005dfe
--- ps/16/6(1932).py
+++ ps/16/6(1932).py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import sys
 
 a = int(sys.stdin.readline())
@@ -15,24 +14,3 @@ for i in range(1, a):
         l[i][j] = max(l[i-1][j-1]+l[i][j],l[i-1][j]+l[i][j])
 
 print(max(max(l)))
-=======
-tri_f = int(input())
-
-tri_a = []
-for i in range(tri_f):
-    tri_a.append(list(map(int, input().split())))
-
-s = 0
-f = 1
-sum_list = []
-for i in range(tri_f):
-    for j in range(f):
-        if i == 0:
-            s = tri_a[i][i]
-        elif i >= 1 and j
-
-    f += 1
-
-
-print(tri_a)
->>>>>>> 8893303b5576737fdd24129b31c136a7fd005dfe
